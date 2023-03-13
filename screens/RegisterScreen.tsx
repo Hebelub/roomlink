0714d1c83b1dff5831ea9476f7f544e5f3fff64c
Gabriel Løsnesløkken <gabriell@uia.no>
2023-03-13 12:33:22 +0100

    Create More of the Register screen

--- screens/RegisterScreen.tsx
+++ screens/RegisterScreen.tsx
@@ -1,6 +1,5 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-
+import { KeyboardAvoidingView, StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
 import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
 import { CompositeNavigationProp, RouteProp, useNavigation, useRoute } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
@@ -9,22 +8,88 @@ import { useTailwind } from 'tailwind-rn/dist';
 import useUsers from '../hooks/useUsers';
 import { RootStackParamList } from '../navigator/RootNavigator';
 import { TabStackParamList } from '../navigator/TabNavigator';
+import { StatusBar } from "expo-status-bar";
+import { Button, Input } from '@rneui/base';
 
 export type ModalScreenNavigationProp = CompositeNavigationProp<
     BottomTabNavigationProp<TabStackParamList>,
     NativeStackNavigationProp<RootStackParamList, "Register">
 >;
 
-
-
 const RegisterScreen = () => {
+
+    const navigation = useNavigation<ModalScreenNavigationProp>();
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [imageUrl, setImageUrl] = useState("");
+
+    const register = () => {
+
+    }
+
     return (
-        <View>
-            <Text>RegisterScreen</Text>
-        </View>
+        <KeyboardAvoidingView behavior='padding' style={styles.container}>
+            <StatusBar style='light' />
+
+            <Text style={{ marginBottom: 50 }}>
+                Create a Roomlink account
+            </Text>
+
+            <View style={styles.container}>
+                <Input
+                    style={styles.input}
+                    placeholder='Username'
+                    autoFocus
+                    value={name}
+                    onChangeText={(text) => setName(text)}
+                />
+                <Input
+                    style={styles.input}
+                    placeholder='Email'
+                    value={email}
+                    onChangeText={(text) => setEmail(text)}
+                />
+                <Input
+                    style={styles.input}
+                    placeholder='Password'
+                    secureTextEntry
+                    value={password}
+                    onChangeText={(text) => setPassword(text)}
+                />
+                <Input
+                    style={styles.input}
+                    placeholder='ImageUrl (optional)'
+                    value={imageUrl}
+                    onChangeText={(text) => setImageUrl(text)}
+                    onSubmitEditing={register}
+                />
+            </View>
+
+            <Button style={styles.button} onPress={register} title="Register"></Button>
+
+        </KeyboardAvoidingView>
     )
 }
 
 export default RegisterScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    input: {
+        height: 50,
+        width: 300,
+        margin: 10,
+        borderWidth: 1,
+        padding: 10,
+    },
+    button: {
+        width: 200,
+        marginTop: 10,
+    },
+})
