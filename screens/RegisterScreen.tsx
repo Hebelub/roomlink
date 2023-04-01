e91920d4caf3fcdecf0aee9ac335483c920c7238
Gabriel Løsnesløkken <gabriell@uia.no>
2023-04-01 15:44:30 +0200

    Format RegisterScreen

--- screens/RegisterScreen.tsx
+++ screens/RegisterScreen.tsx
@@ -1,19 +1,19 @@
 import {
-  ScrollView,
-  KeyboardAvoidingView,
-  StyleSheet,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  View,
+    ScrollView,
+    KeyboardAvoidingView,
+    StyleSheet,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    View,
 } from "react-native";
 import React, { useState } from "react";
 import { BottomTabNavigationProp } from "@react-navigation/bottom-tabs";
 import {
-  CompositeNavigationProp,
-  RouteProp,
-  useNavigation,
-  useRoute,
+    CompositeNavigationProp,
+    RouteProp,
+    useNavigation,
+    useRoute,
 } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { Icon } from "@rneui/themed";
@@ -26,116 +26,116 @@ import { Button, Input } from "@rneui/base";
 import { db, auth, addDoc, collection } from "../firebase";
 
 export type ModalScreenNavigationProp = CompositeNavigationProp<
-  BottomTabNavigationProp<TabStackParamList>,
-  NativeStackNavigationProp<RootStackParamList, "Register">
+    BottomTabNavigationProp<TabStackParamList>,
+    NativeStackNavigationProp<RootStackParamList, "Register">
 >;
 
 const RegisterScreen = () => {
-  const navigation = useNavigation<ModalScreenNavigationProp>();
+    const navigation = useNavigation<ModalScreenNavigationProp>();
 
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [imageUrl, setImageUrl] = useState("");
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [imageUrl, setImageUrl] = useState("");
 
-  const register = async () => {
-    try {
-      const docRef = await addDoc(collection(db, "users"), {
-        name: name,
-        email: email,
-        password: password,
-        imageUrl: imageUrl,
-      });
+    const register = async () => {
+        try {
+            const docRef = await addDoc(collection(db, "users"), {
+                name: name,
+                email: email,
+                password: password,
+                imageUrl: imageUrl,
+            });
 
-      console.log("Document written with ID: ", docRef.id);
-      setName("");
-      setEmail("");
-      setPassword("");
-      setImageUrl("");
-    } catch (e) {
-      console.error("Error adding document: ", e);
-    }
-  };
+            console.log("Document written with ID: ", docRef.id);
+            setName("");
+            setEmail("");
+            setPassword("");
+            setImageUrl("");
+        } catch (e) {
+            console.error("Error adding document: ", e);
+        }
+    };
 
-  return (
-    <KeyboardAvoidingView style={styles.container} behavior="height">
-      <Text style={styles.title}>Create a Roomlink account</Text>
-      <View style={{ flex: 1 }}>
-        <ScrollView contentContainerStyle={styles.form}>
-          <TextInput
-            style={styles.input}
-            placeholder="Name"
-            value={name}
-            onChangeText={(text) => setName(text)}
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Email"
-            value={email}
-            onChangeText={(text) => setEmail(text)}
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Password"
-            secureTextEntry
-            value={password}
-            onChangeText={(text) => setPassword(text)}
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Profile Image URL (optional)"
-            value={imageUrl}
-            onChangeText={(text) => setImageUrl(text)}
-            onSubmitEditing={register}
-          />
-          <TouchableOpacity style={styles.button} onPress={register}>
-            <Text style={styles.buttonText}>Register</Text>
-          </TouchableOpacity>
-        </ScrollView>
-      </View>
-    </KeyboardAvoidingView>
-  );
+    return (
+        <KeyboardAvoidingView style={styles.container} behavior="height">
+            <Text style={styles.title}>Create a Roomlink account</Text>
+            <View style={{ flex: 1 }}>
+                <ScrollView contentContainerStyle={styles.form}>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Name"
+                        value={name}
+                        onChangeText={(text) => setName(text)}
+                    />
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Email"
+                        value={email}
+                        onChangeText={(text) => setEmail(text)}
+                    />
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Password"
+                        secureTextEntry
+                        value={password}
+                        onChangeText={(text) => setPassword(text)}
+                    />
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Profile Image URL (optional)"
+                        value={imageUrl}
+                        onChangeText={(text) => setImageUrl(text)}
+                        onSubmitEditing={register}
+                    />
+                    <TouchableOpacity style={styles.button} onPress={register}>
+                        <Text style={styles.buttonText}>Register</Text>
+                    </TouchableOpacity>
+                </ScrollView>
+            </View>
+        </KeyboardAvoidingView>
+    );
 };
 
 export default RegisterScreen;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-    padding: 20,
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 50,
-  },
-  form: {
-    width: "100%",
-    maxWidth: 350,
-  },
-  input: {
-    backgroundColor: "#fff",
-    borderColor: "#ccc",
-    borderWidth: 1,
-    borderRadius: 5,
-    height: 50,
-    padding: 10,
-    marginBottom: 10,
-    fontSize: 16,
-  },
-  button: {
-    backgroundColor: "#007AFF",
-    padding: 15,
-    borderRadius: 5,
-    marginTop: 20,
-  },
-  buttonText: {
-    color: "#fff",
-    fontSize: 18,
-    fontWeight: "bold",
-    textAlign: "center",
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 20,
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: "bold",
+        marginBottom: 50,
+    },
+    form: {
+        width: "100%",
+        maxWidth: 350,
+    },
+    input: {
+        backgroundColor: "#fff",
+        borderColor: "#ccc",
+        borderWidth: 1,
+        borderRadius: 5,
+        height: 50,
+        padding: 10,
+        marginBottom: 10,
+        fontSize: 16,
+    },
+    button: {
+        backgroundColor: "#007AFF",
+        padding: 15,
+        borderRadius: 5,
+        marginTop: 20,
+    },
+    buttonText: {
+        color: "#fff",
+        fontSize: 18,
+        fontWeight: "bold",
+        textAlign: "center",
+    },
 });
