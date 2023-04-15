ce7be017952cc83ba9c4688157ad77c796a586a3
Gabriel Løsnesløkken <gabriell@uia.no>
2023-04-15 23:25:59 +0200

    Format code

--- components/ChatMessage.tsx
+++ components/ChatMessage.tsx
@@ -5,81 +5,77 @@ import { RoomNavigatorScreenNavigationProp } from "../navigator/RoomNavigator";
 import { Message } from "../types";
 import { dark } from "@mui/material/styles/createPalette";
 
-interface ChatMessageProps {
-  text: string;
-  createdBy: string;
-  createdAt: Date;
+type ChatMessageProps = {
+	text: string;
+	createdBy: string;
+	createdAt: Date;
 }
 
-const ChatMessage: React.FC<ChatMessageProps> = ({
-  text,
-  createdBy,
-  createdAt,
-}) => {
-  const navigation = useNavigation<RoomNavigatorScreenNavigationProp>();
+const ChatMessage = ({ text, createdBy, createdAt }: Message) => {
+	const navigation = useNavigation<RoomNavigatorScreenNavigationProp>();
 
-  return (
-    <SafeAreaView>
-      <ScrollView style={styles.container}>
-        <View style={styles.messagecontainer}>
-          <View style={styles.avatar}>
-            <Text style={styles.avatarText}>{createdBy.charAt(0)}</Text>
-          </View>
-          <View style={styles.messageContainer}>
-            <Text style={styles.messageText}>{text}</Text>
-            <Text style={styles.dateText}>{createdAt.toDateString()}</Text>
-          </View>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+	return (
+		<SafeAreaView>
+			<ScrollView style={styles.container}>
+				<View style={styles.messagecontainer}>
+					<View style={styles.avatar}>
+						<Text style={styles.avatarText}>{createdBy.charAt(0)}</Text>
+					</View>
+					<View style={styles.messageContainer}>
+						<Text style={styles.messageText}>{text}</Text>
+						<Text style={styles.dateText}>{createdAt.toDateString()}</Text>
+					</View>
+				</View>
+			</ScrollView>
+		</SafeAreaView>
+	);
 };
 
 const styles = StyleSheet.create({
-  container: {},
-  messagecontainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    marginVertical: 8,
-    // backgroundColor: "#F7FAFC",
-  },
-  avatar: {
-    backgroundColor: "#E2E8F0",
-    borderRadius: 9999,
-    padding: 12,
-  },
-  avatarText: {
-    fontWeight: "bold",
-    fontSize: 20,
-    color: "#4A5568",
-  },
-  messageContainer: {
-    marginLeft: 16,
-    marginRight: 8,
-    paddingVertical: 12,
-    paddingHorizontal: 16,
-    backgroundColor: "#F7FAFC",
-    borderRadius: 8,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 1,
-    },
-    shadowOpacity: 0.2,
-    shadowRadius: 1.41,
-    elevation: 2,
-    flex: 1,
-  },
-  messageText: {
-    fontWeight: "bold",
-    fontSize: 16,
-    color: "#1A202C",
-  },
-  dateText: {
-    fontSize: 12,
-    color: "#4A5568",
-    marginTop: 4,
-  },
+	container: {},
+	messagecontainer: {
+		flexDirection: "row",
+		alignItems: "center",
+		marginVertical: 8,
+		// backgroundColor: "#F7FAFC",
+	},
+	avatar: {
+		backgroundColor: "#E2E8F0",
+		borderRadius: 9999,
+		padding: 12,
+	},
+	avatarText: {
+		fontWeight: "bold",
+		fontSize: 20,
+		color: "#4A5568",
+	},
+	messageContainer: {
+		marginLeft: 16,
+		marginRight: 8,
+		paddingVertical: 12,
+		paddingHorizontal: 16,
+		backgroundColor: "#F7FAFC",
+		borderRadius: 8,
+		shadowColor: "#000",
+		shadowOffset: {
+			width: 0,
+			height: 1,
+		},
+		shadowOpacity: 0.2,
+		shadowRadius: 1.41,
+		elevation: 2,
+		flex: 1,
+	},
+	messageText: {
+		fontWeight: "bold",
+		fontSize: 16,
+		color: "#1A202C",
+	},
+	dateText: {
+		fontSize: 12,
+		color: "#4A5568",
+		marginTop: 4,
+	},
 });
 
 export default ChatMessage;
