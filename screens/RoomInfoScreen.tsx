f3004639d8d2f538b820495ee9430250e5e5dcd3
teklitag <teklitag@uia.no>
2023-04-20 00:19:23 +0200

    Resolve merge comflict

--- screens/RoomInfoScreen.tsx
+++ screens/RoomInfoScreen.tsx
@@ -1,4 +1,5 @@
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+
+import { StyleSheet, Text, View, TouchableOpacity, ScrollView } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { RoomNavigatorRouteProp, RoomNavigatorScreenNavigationProp } from '../navigator/RoomNavigator';
 import { RouteProp, useNavigation, useRoute } from '@react-navigation/native';
@@ -29,28 +30,31 @@ const RoomInfoScreen = () => {
     const user = useUser(roomProps.createdById);
 
     return (
-        <View style={styles.container}>
+        <ScrollView style={{ backgroundColor: 'lightgreen' }}>
+            <View style={styles.container}>
+                <View style={styles.headerContainer} >
+                    <Text style={styles.header}>{roomProps.name}</Text>
+                </View>
 
-            <Text style={styles.header}>{roomProps.name}</Text>
+                <View style={styles.spacing} />
+                <View style={styles.descriptionContainer}>
+                    <Text style={styles.description}>{roomProps.description}</Text>
+                </View>
+                <View style={styles.spacing} />
 
-            <View style={styles.spacing} />
-            <View style={styles.descriptionContainer}>
-                <Text style={styles.description}>{roomProps.description}</Text>
+                {/* <Text>The room was created at {roomProps.createdAt.toDateString()}</Text> */}
+
+                <TouchableOpacity onPress={() => (Clipboard.setStringAsync(roomProps.code))} style={styles.qrContainer}>
+                    <Text style={styles.codeText}>{roomProps.code}</Text>
+                    <RoomQrCode code={roomProps.code} />
+                </TouchableOpacity>
+
+                <View style={styles.CreatedContainer}>
+                    <Text style={styles.createdBy}>Created By </Text>
+                    <Text style={styles.displayName}>{user?.displayName}</Text>
+                </View>
             </View>
-            <View style={styles.spacing} />
-
-            {/* <Text>The room was created at {roomProps.createdAt.toDateString()}</Text> */}
-
-            <TouchableOpacity onPress={() => (Clipboard.setStringAsync(roomProps.code))} style={styles.qrContainer}>
-                <Text style={styles.codeText}>{roomProps.code}</Text>
-                <RoomQrCode code={roomProps.code} />
-            </TouchableOpacity>
-
-            <View style={styles.CreatedContainer}>
-                <Text style={styles.createdBy}>Created By </Text>
-                <Text style={styles.displayName}>{user?.displayName}</Text>
-            </View>
-        </View>
+        </ScrollView>
     )
 }
 
@@ -63,41 +67,40 @@ const styles = StyleSheet.create({
         paddingHorizontal: 20,
         paddingTop: 20,
     },
+    headerContainer: {
+
+    },
     header: {
-        fontSize: 60,
-        fontWeight: 'bold',
+        shadowOpacity: 0.1,
+        shadowOffset: {
+            width: 0.1,
+            height: 0.2,
+        },
+        fontSize: 50,
+        fontWeight: '800',
         textAlign: 'center',
+        padding: 1,
     },
     description: {
-        fontSize: 24,
+        fontSize: 18,
         lineHeight: 24,
         textAlign: 'center',
-        color: '#333', // set color to black or your preferred color
-        marginTop: 10, // add margin to separate from other elements
+        color: '#333',
+        marginTop: 1,
+        padding: 2,
+        fontWeight: '200',
 
     },
 
     codeText: {
-        fontSize: 60,
+        fontSize: 40,
         fontWeight: 'bold',
         textAlign: 'center',
         marginBottom: 10,
     },
 
     descriptionContainer: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#fff',
-        borderRadius: 20,
-        padding: 10,
-        shadowColor: '#000',
-        shadowOpacity: 0.7,
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-        shadowRadius: 4,
-        elevation: 5,
+
     },
     qrContainer: {
         alignItems: 'center',
@@ -108,7 +111,7 @@ const styles = StyleSheet.create({
         shadowColor: '#000',
         shadowOpacity: 0.7,
         shadowOffset: {
-            width: 0,
+            width: 1,
             height: 2,
         },
         shadowRadius: 4,
@@ -119,24 +122,32 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
 
-        borderRadius: 20,
+        padding: 10,
 
-
+        shadowOpacity: 0.1,
+        shadowOffset: {
+            width: 0.2,
+            height: 0.3,
+        },
+        shadowRadius: 4,
+        elevation: 5,
         marginTop: 40,
     },
 
     createdBy: {
         // styles for the "Created By" text
-        fontSize: 38,
-        fontWeight: 'bold',
+        fontSize: 28,
+        fontWeight: '400',
         color: '#333',
         marginBottom: 10,
+
     },
     displayName: {
         // styles for the user's display name
-        fontSize: 24,
-        fontWeight: 'bold',
+        fontSize: 60,
+        fontWeight: '200',
         color: '#333',
+
     },
     spacing: {
         height: 20,
